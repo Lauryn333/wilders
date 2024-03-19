ad124be06e4fd2f46f0c830a93ebff5b7601fb57
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-03-19 11:32:09 +0100

    Ajout de tous mes fichiers

---
+++ index.js
@@ -0,0 +1,12 @@
+const name1 = "Amel";
+const name2 = "Mégane";
+const name3 = "Maéva";
+const name4 = "Rachel";
+
+import chalk from 'chalk';
+
+console.log(`Wilders are :`);
+console.log(chalk.blue(name1));
+console.log(chalk.bgYellow(name2));
+console.log(chalk.bgGreen(name3));
+console.log(chalk.cyanBright(name4));
---
+++ information.js
@@ -0,0 +1,13 @@
+const name1 = "Amel";
+const name2 = "Mégane";
+const name3 = "Maéva";
+const name4 = "Rachel";
+
+
+module.exports = {
+    name1: name1,
+    name2: name2,
+    name3: name3,
+    name4: name4,
+
+};
